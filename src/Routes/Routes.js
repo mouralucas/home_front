fcbbc2b367a2d9e502e8550dae0904cbf62d06cc
Lucas Moura <lucas.moura@inteliger.com.br>
2022-08-20 00:01:32 -0300

    Criado login como class component e setado background somente para uma pagina

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import Home from '../Pages/Library/Home'
+import Login from "../Pages/Login";
 
 function HomeRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<Home />} path=""   />
+                <Route element={<Home />} path="/library/home"   />
+                <Route element={<Login />} path="" />
             </Routes>
         </BrowserRouter>
     );
