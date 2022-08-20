7616efa481d3f0ac377f2c0e701dfc279abebfb6
Lucas Moura <lucaspenha471@gmail.com>
2022-08-20 14:33:38 -0300

    Adicionado teste de token na home

--- src/Pages/Library/Home.js
+++ src/Pages/Library/Home.js
@@ -2,8 +2,17 @@ import Header from "../../Components/Header";
 import MyChart from "../../Components/Chart";
 import Button from '../../Components/Button'
 import Navbar from '../../Components/Navbar'
+import {useState} from "react";
+import Login from '../Login'
+
 
 function Home () {
+    const [token, setToken] = useState()
+
+    if (!token) {
+        return <Login />
+    }
+
     return (
         <div className="App">
             {/*<Header />*/}
