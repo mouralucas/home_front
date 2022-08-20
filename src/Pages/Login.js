ac28476b219ba3d0c658b716feaab3fec21cdaa8
Lucas Moura <lucas.moura@inteliger.com.br>
2022-08-20 10:05:09 -0300

    Criado css para login Atualizado a forma de definir a cor de fundo do login, sem afetar o resto do projeto

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,16 +1,31 @@
 import React from "react";
 import Header from "../Components/Header";
-
+import {Link} from 'react-router-dom'
+import '../Assets/Login/Login.css'
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
         console.log(this.props)
-        document.body.style.backgroundColor = '#00FF7F'
     }
 
     render() {
-        return <Header />
+        return (
+            <div className='form-login'>
+                <div className="">
+                    <form action="">
+                        <label htmlFor="">Username</label>
+                        <input type="text"/>
+                        <label htmlFor="">Password</label>
+                        <input type="password"/>
+                    </form>
+                    <p class="mt-4 text-center">
+                        Não consegue acessar?
+                        <Link to='/library/home'> Clique aqui </Link>
+                    </p>
+                </div>
+            </div>
+        );
     }
 }
 
