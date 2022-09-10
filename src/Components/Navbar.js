c4c1ae9abc218566c3c08a651041592768620d96
Lucas Moura <lucas.moura@inteliger.com>
2022-09-10 16:44:16 -0300

    Criacao de novo navbar (ainda nao final)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,42 +1,46 @@
-import React from "react";
+import {useState} from "react"
+import "../Assets/Core/Components/Navbar.css"
 import {Link} from "react-router-dom";
-import {logout} from "../Services/Auth/Auth";
-import logo from '../Assets/Core/Images/Logo/logo_lucas.svg';
-import '../Assets/Core/Components/Navbar.css'
 
-const App = (props) => {
+export default function Navbar() {
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+
     return (
-        <div class="container">
-            <nav class="navbar bg-dark">
-                <div class="container">
-                    <a class="navbar-brand" href={'www.google.com'}>
-                        <img src={logo} alt={'Logo'}/>
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home
-                                    <span class="sr-only">(current)</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Profile</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Impressum</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <nav className="navigation">
+            <a href="/" className="brand-name">
+                MacroSoft
+            </a>
+            <button className="nav-button" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="white"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </button>
+            <div
+                className={
+                    isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+                }
+            >
+                <ul>
+                    <li>
+                        <Link to="/library/home">Biblioteca</Link>
+                    </li>
+                    <li>
+                        <Link to="/finance/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/finance/records">Registros</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
-
-export default App;
