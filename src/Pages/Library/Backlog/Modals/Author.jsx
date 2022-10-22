229ddf77e624db8b65702ffdabbe36ad80b1c9fa
Lucas Penha de Moura <lucaspenha471@gmail.com>
2022-10-22 09:06:06 -0300

    Adicionado modal de autor

---
+++ src/Pages/Library/Backlog/Modals/Author.jsx
@@ -0,0 +1,143 @@
+import Modal from "../../../../Components/Modal";
+import {useEffect, useState} from "react";
+import {URL_ACCOUNTS, URL_CATEGORIES, URL_COUNTRY, URL_LANGUAGE, URL_STATEMENT} from "../../../../Services/Axios/ApiUrls";
+import Currency from "../../../../Components/Currency";
+import DateBox from "devextreme-react/date-box";
+import Moment from "moment/moment";
+import handleSubmit from '../../../../Services/Axios/Post'
+import AsyncSelect from "react-select/async";
+import filterSelect from "../../../../Utils/DataHandling";
+import {getData} from "../../../../Services/Axios/Get";
+
+
+const App = (props) => {
+    const [country, setCountry] = useState([])
+    const [selectedCountry, setSelectedCountry] = useState()
+
+    const [language, setLanguage] = useState([]);
+    const [selectedLanguage, setSelectedLanguage] = useState();
+
+    const [author, setAuthor] = useState([]);
+    const [selectedAuthor, setSelectedAuthor] = useState();
+
+    const [values, setValues] = useState({})
+
+    useEffect(() => {
+        if (props.author && props.modalState) {
+            setValues(props.statement);
+        }
+
+        if (!props.modalState) {
+            setValues({
+                nm_full: null,
+                country_id: 0,
+                language_id: 0,
+                description: '',
+            });
+            setSelectedCountry(null);
+            setSelectedLanguage(null);
+        }
+    }, [props.modalState, props.author])
+
+    useEffect(() => {
+        if (props.statement) {
+            setSelectedAuthor(author.filter(i => i.value === props.statement.account_id)[0]);
+        }
+    }, [author, props.statement])
+
+    const getCountry = (query, callback) => {
+        if (query) {
+            callback(filterSelect(country, query));
+        } else {
+            getData(URL_COUNTRY).then(response => {
+                let options = response == null ? {} : response.countries.map(i => ({value: i.id, label: i.name}))
+                callback(options);
+                setSelectedCountry(options.filter(category => category.value === values.category_id)[0]);
+                setCountry(options);
+            });
+        }
+    }
+
+    const getLanguage = (query, callback) => {
+        if (query) {
+            callback(filterSelect(language, query));
+        } else {
+            getData(URL_LANGUAGE).then(response => {
+                let options = response.languages.map(i => ({value: i.id, label: i.name}));
+                callback(options);
+
+                setLanguage(options);
+                setSelectedLanguage(options.filter(i => i.value === values.language_id)[0]);
+            });
+        }
+    }
+
+    const set = name => {
+        return ({target: {value}}) => {
+            setValues(oldValues => ({...oldValues, [name]: value}));
+        }
+    }
+
+    const setCombo = (e, name, setFunction) => {
+        if (e !== null) {
+            setFunction(e);
+            return setValues(oldValues => ({...oldValues, [name]: e.value}));
+        }
+        return setValues(oldValues => ({...oldValues, [name]: e.value}));
+    }
+
+    const setDate = (e, name) => {
+        return setValues(oldValues => ({...oldValues, [name]: Moment(e.value).format('YYYY-MM-DD')}))
+    }
+
+    const body = () => {
+        let body_html =
+            <form>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-3">
+                            <label htmlFor="">Nome: {values.nm_full}</label>
+                            <input type="text" className="form-control input-default"/>
+                        </div>
+                        <div className="col-3">
+                            <label htmlFor="">Data nascimento</label>
+                            <DateBox value={values.dat_purchase} type="date" className='form-control input-default'
+                                     onValueChanged={(date) => setDate(date, 'dat_purchase')}/>
+                        </div>
+                        <div className="col-3">
+                            <label htmlFor="">Country {values.country_id}</label>
+                            <AsyncSelect formTarget={true} loadOptions={(query, callback) => getCountry(query, callback)} onChange={(e) => setCombo(e, 'country_id', setSelectedCountry())} defaultOptions value={selectedCountry}/>
+                        </div>
+                        <div className="col-3">
+                            <label htmlFor="">Idioma {values.language_id}</label>
+                            <AsyncSelect formTarget={true} loadOptions={(query, callback) => getLanguage(query, callback)} onChange={(e) => setCombo(e, 'language_id', setSelectedLanguage)} defaultOptions value={selectedLanguage}/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <label htmlFor="">Descrição</label>
+                            <textarea className='form-control' value={values.description} id="" cols="30" rows="5" onChange={set('description')}></textarea>
+                        </div>
+                    </div>
+                </div>
+            </form>;
+
+        return body_html
+    }
+
+    return (
+        <div>
+            <Modal
+                showModal={props.modalState}
+                hideModal={props.hideModal}
+                title={'Extrato'}
+                body={body()}
+                fullscreen={false}
+                actionModal={(e) => handleSubmit(e, URL_STATEMENT, values)}
+                size={'lg'}
+            />
+        </div>
+    );
+}
+
+export default App;
