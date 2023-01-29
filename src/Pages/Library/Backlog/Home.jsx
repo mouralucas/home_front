412879ce32f553078f57a3e7e653be3114138244
Lucas Moura <lucaspenha471@gmail.com>
2023-01-28 22:49:36 -0300

    Sidebar set as component (not functional)

--- src/Pages/Library/Backlog/Home.jsx
+++ src/Pages/Library/Backlog/Home.jsx
@@ -1,12 +1,7 @@
-import Card from "../../../Components/Card";
-import AuthorTable from './Tables/Author'
-import PublisherTable from './Tables/Publisher'
-import SerieTable from './Tables/Serie'
-import CollectionTable from './Tables/Collection'
-import {Sidebar, Menu, MenuItem, useProSidebar, SubMenu, sidebarClasses} from 'react-pro-sidebar';
-import {Badge} from "react-bootstrap";
-import {BarChart} from "../../../Assets/Core/Images/icons/BarChart";
-import {SidebarFooter} from "../../../Assets/Core/Components/Sidebar/Footer";
+import Sidebar from '../../../Components/Sidebar/Base';
+import AuthorTable from "./Tables/Author";
+import React from "react";
+import Card from '../../../Components/Card'
 
 
 // Sidebar documentation:
@@ -14,53 +9,26 @@ import {SidebarFooter} from "../../../Assets/Core/Components/Sidebar/Footer";
 const App = () => {
     return (
         <>
-            <div style={{display: 'flex', height: '100%'}}>
-                <Sidebar defaultCollapsed={true}
-                         transitionDuration={2000}>
-                    <Menu
-                        rootStyles={{
-                            [`.${sidebarClasses.container}`]: {
-                                backgroundColor: 'red',
-                            },
-                        }}
-                    >
-                        <MenuItem
-                            icon={<BarChart/>}
-                        >Item</MenuItem>
-                        <SubMenu
-                            label="Charts"
-                            icon={<BarChart/>}
-                            suffix={
-                                <Badge variant="danger" shape="square">
-                                    6
-                                </Badge>
-                            }>
-                            <MenuItem> Pie charts </MenuItem>
-                            <MenuItem> Line charts </MenuItem>
-                        </SubMenu>
-                    </Menu>
-                    <SidebarFooter collapsed={true}/>
-                </Sidebar>
-                <main>
-                    <div className='App'>
-                        <div className="row">
-                            <div className="col-12">
-                                <Card>
-                                    <Card.Header>
-                                        <div className="col-6">Autores</div>
-                                        <div className="col-4"></div>
-                                        <div className="col-2">
-                                        </div>
-                                    </Card.Header>
-                                    <Card.Body>
-                                        <AuthorTable/>
-                                    </Card.Body>
-                                </Card>
-                            </div>
+            <Sidebar/>
+            <main>
+                <div className='App'>
+                    <div className="row">
+                        <div className="col-12">
+                            <Card>
+                                <Card.Header>
+                                    <div className="col-6">Autores</div>
+                                    <div className="col-4"></div>
+                                    <div className="col-2">
+                                    </div>
+                                </Card.Header>
+                                <Card.Body>
+                                    <AuthorTable/>
+                                </Card.Body>
+                            </Card>
                         </div>
                     </div>
-                </main>
-            </div>
+                </div>
+            </main>
         </>
     )
 }
