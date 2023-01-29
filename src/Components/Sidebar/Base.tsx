70c02d6a9fc11e469ff12f0d84ac5f3ea63ac0ca
Lucas Moura <lucaspenha471@gmail.com>
2023-01-28 22:50:14 -0300

    Sidebar set as component (not functional)

---
+++ src/Components/Sidebar/Base.tsx
@@ -0,0 +1,46 @@
+import {Menu, MenuItem, Sidebar, sidebarClasses, SubMenu} from "react-pro-sidebar";
+import {BarChart} from "../../Assets/Core/Images/icons/BarChart";
+import {Badge} from "react-bootstrap";
+import {SidebarFooter} from "../../Assets/Core/Components/Sidebar/Footer";
+import AuthorTable from "../../Pages/Library/Backlog/Tables/Author";
+import React from "react";
+import Card from '../../Components/Card'
+
+
+const App = () => {
+    return (
+        <>
+            <div style={{display: 'flex', height: '100%'}}>
+                <Sidebar defaultCollapsed={true}
+                         transitionDuration={2000}>
+                    <Menu
+                        rootStyles={{
+                            [`.${sidebarClasses.container}`]: {
+                                backgroundColor: 'red',
+                            },
+                        }}
+                    >
+                        <MenuItem
+                            icon={<BarChart/>}
+                        >Item</MenuItem>
+                        <SubMenu
+                            label="Charts"
+                            icon={<BarChart/>}
+                            suffix={
+                                <Badge>
+                                    6
+                                </Badge>
+                            }>
+                            <MenuItem> Pie charts </MenuItem>
+                            <MenuItem> Line charts </MenuItem>
+                        </SubMenu>
+                    </Menu>
+                    <SidebarFooter collapsed={true}/>
+                </Sidebar>
+
+            </div>
+        </>
+    );
+}
+
+export default App;
