00b4fe8ab4aca7cb015f055067f52dbde8e3e9dd
Lucas Moura <lucaspenha471@gmail.com>
2023-04-04 09:46:29 -0300

    Add component to create a multlevel navbar

---
+++ src/Components/Navbar/menuItems.ts
@@ -0,0 +1,52 @@
+export const menuItems = [
+    {
+        title: 'Home',
+        url: '/',
+    },
+    {
+        title: 'Services',
+        url: '/services',
+        submenu: [
+            {
+                title: 'Web Development',
+                url: 'web-dev',
+                submenu: [
+                    {
+                        title: 'Frontend',
+                        url: 'frontend',
+                    },
+                    {
+                        title: 'Backend',
+                        submenu: [
+                            {
+                                title: 'NodeJS',
+                                url: 'node',
+                            },
+                            {
+                                title: 'PHP',
+                                url: 'php',
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                title: 'web design',
+                url: 'web-design',
+            },
+            {
+                title: 'web development',
+                url: 'web-dev',
+            },
+            {
+                title: 'SEO',
+                url: 'seo',
+            },
+        ],
+    },
+    {
+        title: 'About',
+        url: '/about',
+    },
+
+];
