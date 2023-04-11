f9f49410c8c8aed31f6df298c4bfef8febbd5065
Lucas Moura <lucaspenha471@gmail.com>
2023-04-10 22:51:39 -0300

    Add more fields to investment modal Create references to combo boxes in investment modal Create get function to fetch investment by id (not functional) Adjust values state to set (almost) all investment parameters

--- src/Pages/Finance/Record/Modals/Investment.tsx
+++ src/Pages/Finance/Record/Modals/Investment.tsx
@@ -1,6 +1,6 @@
 import Modal from '../../../../Components/Modal'
 import React, {useEffect, useState} from "react";
-import {URL_INVESTMENT} from "../../../../Services/Axios/ApiUrls";
+import {URL_FINANCE_BANK, URL_FINANCE_INVESTMENT_TYPE, URL_INVESTMENT} from "../../../../Services/Axios/ApiUrls";
 import DateBox from 'devextreme-react/date-box';
 import Moment from 'moment';
 import Currency from '../../../../Components/Currency'
@@ -9,6 +9,7 @@ import {getData} from "../../../../Services/Axios/Get";
 import filterSelect from "../../../../Utils/DataHandling";
 import {format as formatDate} from "../../../../Utils/DateTime";
 import handleSubmit from "../../../../Services/Axios/Post";
+import {toast} from "react-toastify";
 
 
 /**
@@ -18,15 +19,47 @@ import handleSubmit from "../../../../Services/Axios/Post";
  * @constructor
  */
 const App = (props): JSX.Element => {
+    /*
+    * A ideia do investimento é registrar qualquer novo investimento feito
+    *  Deve apenas conter o valor investido, a evolução vai ser salva na tabela investment_statement
+    * O "parente" indica o primeiro depósito naquele investimento (normalmente só terá um por investimento,
+    *       porém em casos como cofrinhos e ultravioleta os investimentos são recorrentes no mesmo registro) qualquer
+    *       outro depósito será registrado com o id da referência e essa referência tem o valor somado. O primeiro
+    *       registro sempre vai ter duas linhas, o "parente" e o depósito realizado (pensar melhor nisso)
+    * Em investimentos com liquidez antes do vencimento podem haver retiradas parciais, serão novos registros com o parent
+    *       respectivo, subitraindo do total do parent
+    *
+    * */
+
     const [investment, setInvestment] = useState([])
     const [selectedInvestment, setSelectedInvestment] = useState([])
 
+    const [investmentType, setInvestmentType] = useState([])
+    const [selectedInvestmentType, setSelectedInvestmentType] = useState([])
+
+    const [custodian, setCustodian] = useState([])
+    const [selectedCustodian, setSelectedCustodian] = useState([])
+
+    const [cashFlow, setCashFlow] = useState([])
+    const [selectedCashFlow, setSelectedCashFlow] = useState([])
+
+    const [interestRate, setInterestRate] = useState([])
+    const [selectedInterestRate, setSelectedInterestRate] = useState([])
+
     const [values, setValues] = useState({
         investmentId: undefined,
+        date: null,
+
+        price: undefined,
         amount: undefined,
-        dat_payment: undefined,
+
+        datMaturity: undefined,
         dat_purchase: undefined,
         datLastEdited: undefined,
+
+        investmentTypeId: undefined,
+        custodianId: undefined,
+
         datCreated: "",
         description: undefined
     })
@@ -40,10 +73,19 @@ const App = (props): JSX.Element => {
             // TODO: ajustar campos
             setValues({
                 investmentId: null,
+                date: new Date(),
+
+                price: 0,
                 amount: 0,
-                dat_payment: new Date(),
+
+                datMaturity: new Date(),
                 dat_purchase: new Date(),
+
                 description: '',
+
+                investmentTypeId: null,
+                custodianId: null,
+
                 datCreated: null,
                 datLastEdited: null
             });
@@ -63,7 +105,6 @@ const App = (props): JSX.Element => {
             callback(filterSelect(investment, query));
         } else {
             getData(URL_INVESTMENT).then(response => {
-                console.log(response)
                 let options = response === null ? {} : response?.investment.map(i => ({value: i.id, label: i.name}));
                 callback(options);
                 setInvestment(options)
@@ -71,6 +112,78 @@ const App = (props): JSX.Element => {
         }
     }
 
+    const getInvestmentType = (query, callback) => {
+        if (query) {
+            callback(filterSelect(investmentType, query));
+        } else {
+            getData(URL_FINANCE_INVESTMENT_TYPE, {show_mode: 'child'}).then(response => {
+                let options = response === null ? {} : response?.investment_type.map(i => ({
+                    value: i.id,
+                    label: i.name
+                }));
+                callback(options);
+                setInvestmentType(options)
+            }).catch(err => {
+                toast.error('Houve um erro ao buscar os tipos de investimentos')
+            })
+        }
+    }
+
+    const getCustodian = (query, callback) => {
+        if (query) {
+            callback(filterSelect(custodian, query));
+        } else {
+            getData(URL_FINANCE_BANK).then(response => {
+                let options = response === null ? {} : response?.bank.map(i => ({value: i.id, label: i.name}));
+                callback(options);
+                setCustodian(options)
+            }).catch(err => {
+                toast.error('Houve um erro ao buscar os agentes de custódia')
+            })
+        }
+    }
+
+    const getCashFlow = (query, callback) => {
+        if (query) {
+            callback(filterSelect(cashFlow, query));
+        } else {
+            let options = [
+                {value: 'INCOMING', label: 'Entrada'},
+                {value: 'OUTGOING', label: 'Saída'}
+            ]
+            callback(options)
+            // @ts-ignore
+            setSelectedCashFlow(options?.filter(cashFlow => cashFlow.value === values.cashFlowId)[0])
+            setCashFlow(options)
+        }
+    }
+
+    const getInterestRate = (query, callback) => {
+        if (query) {
+            callback(filterSelect(interestRate, query));
+        } else {
+            let options = [
+                {value: 'FLOATING', label: 'Variável'},
+                {value: 'FIXED', label: 'Fixo'}
+            ]
+            callback(options)
+            // @ts-ignore
+            setSelectedInterestRate(options?.filter(interestRate => interestRate.value === values.interestrateId)[0])
+            setInterestRate(options)
+        }
+    }
+
+    const setInvestmentCombo = (e, name, setFunction) => {
+        if (e != null) {
+            setFunction(e)
+            getData(URL_INVESTMENT).then(response => {
+                setValues(response.investment)
+            }).catch(err => {
+                toast.error('Houve um erro ao buscar detalhes do investimento')
+            })
+            return
+        }
+    }
 
     const set = name => {
         return ({target: {value}}) => {
@@ -98,6 +211,7 @@ const App = (props): JSX.Element => {
     const setCurrency = (values, name) => {
         return setValues(oldValues => ({...oldValues, [name]: values.value / 100}));
     }
+
     const body = () => {
         let body_html =
             <form>
@@ -105,7 +219,7 @@ const App = (props): JSX.Element => {
                     <div className="row">
                         <div className="col-12">
                             <label htmlFor="">Investimento</label>
-                            <AsyncSelect id={'combo_cards'}
+                            <AsyncSelect id={'combo_investments'}
                                          loadOptions={(query, callback) => getInvestments(query, callback)}
                                          onChange={(e) => setCombo(e, 'investmentId', setSelectedInvestment)}
                                          defaultOptions
@@ -121,13 +235,22 @@ const App = (props): JSX.Element => {
                             <label htmlFor="">Data compra</label>
                             <DateBox value={values.dat_purchase} type="date" className='form-control input-default'
                                      useMaskBehavior={true}
-                                     onValueChanged={(date) => setDate(date, 'dat_purchase')}/>
+                                     onValueChanged={(date) => setDate(date, 'date')}/>
                         </div>
                     </div>
                     <div className='row'>
                         <div className="col-4">
                             <label htmlFor="">Quantidade</label>
-                            <input type="text" className={'form-control input-default'}/>
+                            <input type={'number'} className={'form-control input-default'}/>
+                        </div>
+                        <div className="col-4">
+                            <label htmlFor="">Preço {values.price}</label>
+                            <Currency className='form-control input-default'
+                                      value={values.amount * 100}
+                                      onFocus={event => event.target.select()}
+                                      onValueChange={(values, sourceInfo) => {
+                                          setCurrency(values, 'price')
+                                      }}/>
                         </div>
                         <div className="col-4">
                             <label htmlFor="">Valor: {values.amount}</label>
@@ -138,13 +261,51 @@ const App = (props): JSX.Element => {
                                           setCurrency(values, 'amount')
                                       }}/>
                         </div>
-
                     </div>
                     <div className="row">
-                        <div className="col-12">
-                            <label htmlFor="">Descrição</label>
-                            <textarea className='form-control' value={values.description} id="" cols={30} rows={10}
-                                      onChange={set('description')}></textarea>
+                        <div className="col-3">
+                            <label htmlFor="">Operação</label>
+                            <AsyncSelect id={'combo_cash_flow'}
+                                         loadOptions={(query, callback) => getCashFlow(query, callback)}
+                                         onChange={(e) => setCombo(e, 'cashFlowId', setSelectedCashFlow)}
+                                         defaultOptions
+                                         value={selectedCashFlow}/>
+                        </div>
+                        <div className="col-3">
+                            <label htmlFor="">Indexação</label>
+                            <AsyncSelect id={'combo_interest_rate'}
+                                         loadOptions={(query, callback) => getInterestRate(query, callback)}
+                                         onChange={(e) => setCombo(e, 'interestRateId', setSelectedInterestRate)}
+                                         defaultOptions
+                                         value={selectedInterestRate}/>
+                        </div>
+                        <div className="col-3">
+                            <label htmlFor="">Índice</label>
+                            <input type="text" className='form-control input-default'/>
+                        </div>
+                        <div className="col-3">
+                            <label htmlFor="">Tipo</label>
+                            <AsyncSelect id={'combo_investment_type'}
+                                         loadOptions={(query, callback) => getInvestmentType(query, callback)}
+                                         onChange={(e) => setCombo(e, 'investmentTypeId', setSelectedInvestmentType)}
+                                         defaultOptions
+                                         value={selectedInvestmentType}/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6">
+                            <label htmlFor="">Data vencimento</label>
+                            <DateBox value={values.datMaturity} type="date" className='form-control input-default'
+                                     useMaskBehavior={true}
+                                     onValueChanged={(date) => setDate(date, 'datMaturity')}/>
+                        </div>
+                        <div className="col-6">
+                            <label htmlFor="">Agente de custódia</label>
+                            <AsyncSelect id={'combo_custodian'}
+                                         loadOptions={(query, callback) => getCustodian(query, callback)}
+                                         onChange={(e) => setCombo(e, 'custodianId', setSelectedCustodian)}
+                                         defaultOptions
+                                         value={selectedCustodian}/>
                         </div>
                     </div>
                     <div className="row mt-2">
