998e9753672440e207051a9ce0e66b6d44cfa6aa
Lucas Moura <lucaspenha471@gmail.com>
2023-04-16 21:46:22 -0300

    Remove unused css classes Add default css row class Change extension from library home page

---
+++ src/Pages/Library/Records/Home.tsx
@@ -0,0 +1,36 @@
+import Card from "../../../Components/Card";
+import TableBook from "./Tables/Book";
+import TableManga from "./Tables/Manga";
+import React, {useEffect} from "react";
+
+
+const Home = () => {
+    useEffect(() => {
+        document.title = 'Biblioteca';
+    }, [])
+
+    return (
+        <div className='page-with-menu'>
+            <div className="App">
+                <div className="row">
+                    <Card>
+                        <Card.Header>Livros</Card.Header>
+                        <Card.Body>
+                            <TableBook/>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="row">
+                    <Card>
+                        <Card.Header>Mangás</Card.Header>
+                        <Card.Body>
+                            <TableManga/>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
