aed2fc4a653fd4bfd8015639fe377415f371bb04
Lucas Moura <lucaspenha471@gmail.com>
2023-09-29 21:51:46 -0300

    Add more props to bar chart

--- src/Components/Chart/Bar.tsx
+++ src/Components/Chart/Bar.tsx
@@ -5,16 +5,26 @@ interface BarProps {
     data: any[]
     argumentField: string
     valueField: string
+    title: string
     axis?: string
     name?: any
+    palette?: string[]
     customizePoint?: any
+    customizeLabel?: any
+    seriesLabel?: {
+        visible: boolean
+        customizeText?: any
+    }
 }
 
 const App = (props: BarProps) => {
     return (
         <Chart id="chart"
+               title={props.title}
                dataSource={props.data}
+               palette={props.palette ?? 'Pastel'}
                customizePoint={props.customizePoint}
+               customizeLabel={props.customizeLabel}
         >
             <Series
                 valueField={props.valueField}
@@ -23,7 +33,11 @@ const App = (props: BarProps) => {
                 name={props.name ?? undefined}
                 type="bar"
                 color="#ffaa66"
-            />
+            >
+                {props.seriesLabel &&
+                    <Label visible={props.seriesLabel.visible}></Label>
+                }
+            </Series>
         </Chart>
     )
 }
